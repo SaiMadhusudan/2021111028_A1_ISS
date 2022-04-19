3f4b66489ada9e29fe05b71ce0145d51e07543ff
Sai Madhusudan Gunda <98998175+SaiMadhusudan@users.noreply.github.com>
2022-04-19 13:46:25 +0530

    Update Q5a.sh

--- 2021111028_A1/Q5/Q5a.sh
+++ 2021111028_A1/Q5/Q5a.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+letters=({A..z})
 declare -a arr
 read arr
 
@@ -9,6 +9,35 @@ for((;i<m;i++))
 do
 echo -n "${arr[0]:m-1-i:1}" 
 done
+echo ""
 
 
-echo ${rarr[*]}
+declare -a word
+word=$arr
+
+n=${#word}
+
+for(( i=0; i<n; i++))
+do
+let next_=$(printf "%d\n" "'"${word[0]:m-1-i:1}"")
+let next_=next_+1
+echo -n "${letters[$next_-65]}"
+done
+
+echo ""
+
+let m=${#arr}
+let m=m/2
+
+let i=0
+for((;i<m;i++))
+do
+echo -n "${arr[0]:m-1-i:1}" 
+done
+
+for((;i<2*m;i++))
+do
+echo -n "${arr[0]:i:1}" 
+done
+
+echo ""
