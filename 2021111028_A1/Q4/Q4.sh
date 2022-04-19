0cd6368bf7a9756ba811c1f965c5e60932351750
Sai Madhusudan Gunda <98998175+SaiMadhusudan@users.noreply.github.com>
2022-04-19 13:55:18 +0530

    Update Q4.sh

--- 2021111028_A1/Q4/Q4.sh
+++ 2021111028_A1/Q4/Q4.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
  
-
 read line
 line=$(printf '%s' "$line" | tr ',' ' ')
 
+declare -a arr
+let i=0
 for word in $line
 do
-echo "${word}"
+arr[i]="${word}"
+let i=i+1
 done
+
+let n=${#arr[*]} 
+let n--
+
+let s=1
+while (($s))
+do 
+  s=0
+   for((i=0;i<n;i++))
+   {  
+      let a=${arr[i]}
+      let b=${arr[i+1]}
+
+      if [ $a -gt $b ]
+      then
+         arr[i+1]=$a
+         arr[i]=$b
+         s=$s+1
+      fi
+   }
+done
+
+echo ${arr[@]}
