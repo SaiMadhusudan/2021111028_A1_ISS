da974016c3fc1fa5e8c7af04c93940bccdb0af01
Sai Madhusudan Gunda <98998175+SaiMadhusudan@users.noreply.github.com>
2022-04-19 17:54:07 +0530

    Update Q1a.sh

--- 2021111028_A1/Q1/Q1a.sh
+++ 2021111028_A1/Q1/Q1a.sh
@@ -2,3 +2,16 @@
 
 out="$(awk 'NF' $1)"
 echo -n "$out" > $1 
+
+echo -n "" > temp.txt
+
+while IFS= read -r line || [ -n "$line" ]; 
+do 
+   if [ $(grep -o -i "$line" temp.txt | wc -l) == "0" ]
+   then 
+        echo "$line"
+        echo "$line" >> temp.txt
+   fi
+done < $1
+
+rm temp.txt
